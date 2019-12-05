d8d0211a76e79096a3062cbf9ba0ea479e24ea0a
Diotr <35659983+Diotr@users.noreply.github.com>
2019-12-05 21:21:34 +0000

    some css added

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -3,14 +3,21 @@ import MyWorkThumbnail from './MyWorkThumbnail';
 import { Container } from 'react-bootstrap';
 import TetrisBackground from '../media/images/tetr.png';
 import sowa from '../media/images/sowa.jpg';
+import bird from '../media/images/bird_silhouette_vector_134154_1920x1080.jpg';
 import '../styling/MyWork.css';
 const MyWork=()=> {
 
 const projects = [
-  {project: 'tetrisgame', image: TetrisBackground, title: 'left', link:'/aboutme'},
-  {project: 'snakegame', image:TetrisBackground, title: 'right', link: 'second'},
+  {project: 'tetrisgame', image: bird, title: 'bird', link:'/aboutme'},
+  {project: 'tetrisgame', image: bird, title: 'bird', link:'/aboutme'},
+  {project: 'tetrisgame', image: bird, title: 'bird', link:'/aboutme'},
 
-  {project: 'something', image:TetrisBackground, title: 'bottom', link: 'third'},
+  {project: 'tetrisgame', image: bird, title: 'bird', link:'/aboutme'},
+  {project: 'tetrisgame', image: TetrisBackground, title: 'left', link:'/aboutme'},//link = link to the page with project
+  {project: 'snakegame', image:TetrisBackground, title: 'right', link: '/resume'},
+
+ {project: 'something', image:TetrisBackground, title: 'bottom', link: 'third'},
+  {project: 'somethingelse', image:TetrisBackground, title: 'bottom2', link: 'fourth'}, 
 
 ];
     return (
--- src/components/MyWorkThumbnail.js
+++ src/components/MyWorkThumbnail.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link, withRouter} from 'react-router-dom';
 import '../styling/MyWorkThumbnail.css';
 import { tsPropertySignature } from '@babel/types';
-
+import {useHistory} from "react-router-dom";
 
 
 const MyWorkThumbnail = props =>{
- 
+ let history = useHistory();
   
-    const handleClick = props=>(
+    const handleClick = ()=>{ //redirect to the project page
+        history.push(props.pr.link);
+       
+        console.log(props)
 
-   
-    
-           console.log(props)
-        );
+    };
  
 
 
 
 
     return( 
-       <div className='myworkthumbnail' onClick={()=>handleClick(props.pr.link)}>
-           <Link to ='/resume'>Resume</Link>
+       <div className='myworkthumbnail' onClick={()=>handleClick(props.pr.link) }>
+        <div>
         <img src ={props.pr.image}/>
-        <p>{props.pr.title}</p>
-       
+        
+     <div className ='toplayer'>
+     <h1 className = 'thumbtext'>{props.pr.title}</h1>
+         </div>
+        </div> 
+        
 
 </div>
+
     );}
 
 export default MyWorkThumbnail;
